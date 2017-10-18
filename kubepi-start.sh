a5120920b7817b39040f5bc08c17f0e668b5cd68
David Bingham <david.r.bingham@gmail.com>
2017-10-18 14:26:21 +0000

    Adding scripts

---
+++ kubepi-start.sh
@@ -0,0 +1,5 @@
+kubeadm reset
+kubeadm init --pod-network-cidr 10.244.0.0/16
+sudo cp /etc/kubernetes/admin.conf $HOME/
+sudo chown $(id -u):$(id -g) $HOME/admin.conf
+export KUBECONFIG=$HOME/admin.conf
