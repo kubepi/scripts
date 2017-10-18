a1421165eaaa2c154f94895376bce54135021a2d
David Bingham <david.r.bingham@gmail.com>
2017-10-18 16:03:34 +0100

    Updating

--- master/kubepi-start.sh
+++ master/kubepi-start.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-sudo su -
-
 kubeadm reset
+kubeadm init --pod-network-cidr 10.244.0.0/16
 
 sudo cp /etc/kubernetes/admin.conf $HOME/
 sudo chown "$(id -u)":"$(id -g)" "$HOME/admin.conf"
 export KUBECONFIG="$HOME/admin.conf"
-
-kubeadm init --pod-network-cidr 10.244.0.0/16
