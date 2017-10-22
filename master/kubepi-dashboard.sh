3391108554d81de403d5a51dfed334505c0a12fb
David Bingham <david.r.bingham@gmail.com>
2017-10-22 21:16:14 +0100

    Updating

--- master/kubepi-dashboard.sh
+++ master/kubepi-dashboard.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-kubectl create secret generic kubernetes-dashboard-certs --from-file=$HOME/certs -n kube-system
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
-
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard-arm-head.yaml
 
 kubectl -n kube-system delete $(kubectl -n kube-system get pod -o name | grep dashboard)
