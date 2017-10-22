22961bc3ed1dbe6aad18c3b02a772c53e7cf742a
David Bingham <david.r.bingham@gmail.com>
2017-10-22 21:27:22 +0100

    Updating

--- master/kubepi-dashboard.sh
+++ master/kubepi-dashboard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -sSL https://rawgit.com/kubernetes/dashboard/master/src/deploy/kubernetes-dashboard.yaml | sed "s/amd64/arm/g" | kubectl create -f -
+curl -sSL https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml | sed "s/amd64/arm/g" | kubectl create -f -
 
 kubectl -n kube-system get service kubernetes-dashboard -o template --template="{{ (index .spec.ports 0).nodePort }}" | xargs echo
