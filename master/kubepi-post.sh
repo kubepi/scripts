2b26537486d569be3ab47cf8a97d76f9d6f6ae95
David Bingham <david.r.bingham@gmail.com>
2017-10-31 10:10:27 +0000

    Updating

--- master/kubepi-post.sh
+++ master/kubepi-post.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # Run as a regular user
 
-cp /etc/kubernetes/admin.conf $HOME/
-chown "$(id -u)":"$(id -g)" "$HOME/admin.conf"
 export KUBECONFIG="$HOME/admin.conf"
--- master/kubepi-start.sh
+++ master/kubepi-start.sh
@@ -4,5 +4,8 @@
 kubeadm reset
 kubeadm init --pod-network-cidr 10.244.0.0/16
 
+cp /etc/kubernetes/admin.conf $HOME/
+chown "$(id -u)":"$(id -g)" "$HOME/admin.conf"
+
 curl -sSL https://rawgit.com/coreos/flannel/v0.7.1/Documentation/kube-flannel-rbac.yml | kubectl create -f -
 curl -sSL https://rawgit.com/coreos/flannel/v0.7.1/Documentation/kube-flannel.yml | sed "s/amd64/arm/g" | kubectl create -f -
