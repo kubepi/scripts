cd6714b90663bf86eff2c2f398e911858d6b49ad
David Bingham <david.r.bingham@gmail.com>
2017-11-05 16:41:23 +0000

    Updating

--- master/kubepi-post.sh
+++ master/kubepi-post.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
-# Run as a regular user
 
+sudo cp /etc/kubernetes/admin.conf $HOME/
+sudo chown "$(id -u)":"$(id -g)" "$HOME/admin.conf"
 export KUBECONFIG="$HOME/admin.conf"
+
+curl -sSL https://rawgit.com/coreos/flannel/v0.7.1/Documentation/kube-flannel-rbac.yml | kubectl create -f -
+curl -sSL https://rawgit.com/coreos/flannel/v0.7.1/Documentation/kube-flannel.yml | sed "s/amd64/arm/g" | kubectl create -f -
+
+# Run the following on all nodes
+sudo iptables -A FORWARD -i cni0 -j ACCEPT
+sudo iptables -A FORWARD -o cni0 -j ACCEPT
+
+# Install the load balancer to expose externally
+#kubectl apply -f https://raw.githubusercontent.com/hypriot/rpi-traefik/master/traefik-k8s-example.yaml
+kubectl apply -f https://raw.githubusercontent.com/containous/traefik/master/examples/k8s/traefik-deployment.yaml
+
+kubectl label node <load balancer-node> nginx-controller=traefik
+kubectl label node black-pearl-master nginx-controller=traefik
+kubectl label node black-pearl-slave-b nginx-controller=traefik
+
+cat > hypriot-ingress.yaml <<EOF
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  name: hypriot
+spec:
+  rules:
+  - http:
+      paths:
+      - path: /
+        backend:
+          serviceName: hypriot
+          servicePort: 80
+EOF
+
+kubectl apply -f hypriot-ingress.yaml
--- master/kubepi-start.sh
+++ master/kubepi-start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # Run as sudo
 
+# The following is required to prevent hanging issue with --apiserver-advertise-address=192.168.1.1
+# sudo nano /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+# --fail-swap-on=false in kubelet. Update the following line to include the parameter:
+# Environment="KUBELET_KUBECONFIG_ARGS=--bootstrap-kubeconfig=/etc/kubernetes/bootstrap-kubelet.conf --kubeconfig=/etc/kubernetes/kubelet.conf --fail-swap-on=false"
+
+systemctl daemon-reload && systemctl restart kubelet
+
 kubeadm reset
+# IP address below is kube-dns server address
+# --apiserver-advertise-address=<wifi-ip-address>
+#kubeadm init --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address=0.0.0.0
 kubeadm init --pod-network-cidr 10.244.0.0/16
-
-cp /etc/kubernetes/admin.conf $HOME/
-chown "$(id -u)":"$(id -g)" "$HOME/admin.conf"
-
-curl -sSL https://rawgit.com/coreos/flannel/v0.7.1/Documentation/kube-flannel-rbac.yml | kubectl create -f -
-curl -sSL https://rawgit.com/coreos/flannel/v0.7.1/Documentation/kube-flannel.yml | sed "s/amd64/arm/g" | kubectl create -f -
